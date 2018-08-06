9b7f2a893bf4ab0866bce0ae2cf15baa25ef4867
Andrew <messerback@hotmail.com>
2018-08-06 19:58:26 +0300

    initial commit

---
+++ App.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import { createStackNavigator, createBottomTabNavigator } from 'react-navigation';
+import WelcomeScreen from './src/screens/WelcomeScreen';
+import AuthScreen from './src/screens/AuthScreen';
+import MapScreen from './src/screens/MapScreen';
+import DeckScreen from './src/screens/DeckScreen';
+import SettingsScreen from './src/screens/SettingsScreen';
+import ReviewScreen from './src/screens/ReviewScreen';
+
+const WelcomeScreenNav = createBottomTabNavigator({
+  welcome: { screen: WelcomeScreen },
+  auth: { screen: AuthScreen },
+  main: {
+    screen: createBottomTabNavigator({
+      map: { screen: MapScreen },
+      deck: { screen: DeckScreen },
+      reviewFlow: {
+        screen: createStackNavigator({
+          review: { screen: ReviewScreen },
+          settings: { screen: SettingsScreen }
+        })
+      }
+    })
+  }
+});
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <WelcomeScreenNav />
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    // flex: 1,
+    // backgroundColor: '#fff',
+    // alignItems: 'center',
+    // justifyContent: 'center',
+  },
+});
---
+++ src/screens/ReviewScreen.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+
+export default class ReviewScreen extends Component {
+  
+  static navigationOptions = { 
+    header: null
+  };
+
+  render() {
+    return (
+      <View>
+        <Text> ReviewScreen </Text>
+        <Text> ReviewScreen </Text>
+        <Text> ReviewScreen </Text>
+        <Text> ReviewScreen </Text>
+        <Text> ReviewScreen </Text>
+        <Text> ReviewScreen </Text>
+        <Text> ReviewScreen </Text>
+        <Text> ReviewScreen </Text>
+      </View>
+    );
+  }
+}
